17f0a067346f0c2049183c5428705a5ccfddc121
Ange Kevin <engineeryeo.a@gmail.com>
2021-04-30 13:50:22 +0200

    first commit

---
+++ src/main/java/fr/istic/taa/jaxrs/domain/Section.java
@@ -0,0 +1,69 @@
+package fr.istic.taa.jaxrs.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Section implements Serializable {
+	
+	
+	private long id ;
+	private List<FicheKanban> fichekanbans ; 
+	private TableauKanban tableaukanban ; 
+	private String libelle ;
+	
+	
+	public Section() {
+		super();
+	}
+
+	public Section(String libelle, TableauKanban tableauKanban) {
+		super();
+		this.tableaukanban = tableauKanban;
+		this.libelle = libelle;
+	}
+	
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+	@OneToMany (mappedBy = "section")
+	public List<FicheKanban> getFichekanbans() {
+		return fichekanbans;
+	}
+	public void setFichekanbans(List<FicheKanban> fichekanbans) {
+		this.fichekanbans = fichekanbans;
+	}
+	
+	
+	@ManyToOne
+	public TableauKanban getTableaukanban() {
+		return tableaukanban;
+	}
+	public void setTableaukanban(TableauKanban tableaukanban) {
+		this.tableaukanban = tableaukanban;
+	}
+	
+	
+
+
+}
---
+++ src/main/java/fr/istic/taa/jaxrs/rest/CollaborateurResource.java
@@ -0,0 +1,72 @@
+package fr.istic.taa.jaxrs.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import fr.istic.taa.jaxrs.dao.generic.AbstractJpaDao;
+import fr.istic.taa.jaxrs.domain.Collaborateur;
+
+@Path("/collaborateur")
+@Produces({ "application/json" })
+public class CollaborateurResource {
+
+	private AbstractJpaDao<Long, Collaborateur> collaborateurDao = new AbstractJpaDao<Long, Collaborateur>(){};
+	
+	
+	@GET
+	@Path("/getAll")
+	@Consumes("application/json")
+	public List<Collaborateur> getAllCollaborateurs() {
+		collaborateurDao.setClazz(Collaborateur.class);
+		return collaborateurDao.findAll();
+	}
+	
+	@GET
+	@Path("/{collaborateurId}")
+	@Consumes("application/json")
+	public Collaborateur getCollaborateurById(@PathParam("collaborateurId") Long collaborateurId) {
+		collaborateurDao.setClazz(Collaborateur.class);
+		return collaborateurDao.findOne(collaborateurId);
+	}
+
+	@POST
+	@Consumes("application/json")
+	public Response addCollaborateur( Collaborateur collab) {
+	
+		collaborateurDao.save(collab);
+
+		return Response.ok().entity("SUCCESS").build();
+	}
+	
+	@PUT
+	@Consumes("application/json")
+	public Response modifyCollaborateur( Collaborateur collab) {
+	
+		collaborateurDao.update(collab);
+
+		return Response.ok().entity("SUCCESS").build();
+	}
+	
+
+	@DELETE
+	@Path("/{collaborateurId}")
+	@Consumes("application/json")
+	public Response deleteCollaborateur(@PathParam("collaborateurId") Long collaborateurId) {
+		collaborateurDao.setClazz(Collaborateur.class);
+		collaborateurDao.deleteById(collaborateurId);
+
+		return Response.ok().entity("SUCCESS").build();
+	}
+	
+	
+	
+}
